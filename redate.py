009281c61d638e6f42f936f98b1b7d8e415af6f4
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-04-20 19:28:04 +0100

    adding the update timespamts

---
+++ redate.py
@@ -0,0 +1,20 @@
+import json
+from datetime import datetime,timedelta
+
+JSON_FILE = "data.json"
+BLOCK = 16
+
+initial_date = datetime(2025, 4, 18, 0, 0, 0)
+timestamp_base = int(initial_date.timestamp())
+
+with open(JSON_FILE,"r") as file:
+    data = json.load(file)
+
+
+for i,item in enumerate(data):
+    group = i // BLOCK
+    item["TimeStamp"] = timestamp_base + group
+    print("Number :",i)
+
+with open("data_updated.json","w") as f:
+    json.dump(data,f,indent=2)
